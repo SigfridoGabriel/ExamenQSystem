79071191ad21b8644beafaabfceaa7d1a85358fa
sigfrido gabriel osorio gallegos <sigfrido_osorio2099@hotmail.com>
2018-03-14 22:57:42 -0500

    examen
    examen

---
+++ Parte02/Controllers/SucursalesController.cs
@@ -0,0 +1,32 @@
+﻿using Parte02.Models;
+using Parte02.Repository;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using System.Xml.Serialization;
+
+namespace Parte02.Controllers
+{
+    public class SucursalesController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage SucursalesPorBanco(string banco)
+        {
+            var conn = ConfigurationManager.ConnectionStrings["Examen"].ToString();
+            var sr = new SucursalesRepository(conn);
+            var retorno = sr.ObtenerSucursalesPorbanco(banco);
+            var serializer = new XmlSerializer(typeof(List<Sucursales>));
+            var stringwriter = new System.IO.StringWriter();
+            serializer.Serialize(stringwriter, retorno);
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent(stringwriter.ToString(), Encoding.UTF8, "application/xml")
+            };
+        }
+    }
+}
---
+++ Parte02/Repository/SucursalesRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Parte02.Repository
+{
+    public class SucursalesRepository
+    {
+        public string DefaultConnection { get; set; }
+
+        public SucursalesRepository(string connection)
+        {
+            this.DefaultConnection = connection;
+        }
+
+        public IList<Models.Sucursales> ObtenerSucursalesPorbanco(string banco)
+        {
+            try
+            {
+                var sucursales = new List<Models.Sucursales>();
+                using (var connection = new SqlConnection(DefaultConnection))
+                {
+                    connection.Open();
+                    var command = new SqlCommand()
+                    {
+                        Connection = connection,
+                        CommandType = CommandType.StoredProcedure,
+                        CommandText = "ObtenerSucursalesPorbanco",
+                        CommandTimeout = 0
+                    };
+
+                    command.Parameters.Add(new SqlParameter("@banco", banco));
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var _sucursales = new Models.Sucursales
+                            {
+                                IdSucursal = Convert.ToInt32(reader["IdSucursal"].ToString()),
+                                Nombre = reader["Nombre"].ToString(),
+                                Direccion = reader["Direccion"].ToString(),
+                                FechaRegistro = Convert.ToDateTime(reader["FechaRegistro"].ToString())
+                            };
+                            sucursales.Add(_sucursales);
+                        }
+                    }
+                }
+
+                return sucursales;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }
+        }
+    }
+}
